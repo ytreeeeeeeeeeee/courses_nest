6ff0cbf102bd992e9fb4263031c18c7e868b7189
Kvaskov <106033849+ytreeeeeeeeeeee@users.noreply.github.com>
2023-12-16 21:13:34 +0300

    Домашнее задание к занятию «2.2. Погружение в Nest.js»

---
+++ library/src/books/books.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { BooksService } from './books.service';
+
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
+}
